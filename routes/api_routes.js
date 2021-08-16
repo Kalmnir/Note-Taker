4204fcbfb19bc69cf89e76590e0061703ab078f9
John-Mark Sackett <jmsackett91@gmail.com>
2021-08-16 18:42:24 -0400

    wrote my get for /notes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
 const notes = require('../db/db.json');
 const api = require('express').Router();
+
+api.get('/notes', (request, response) => {
+    console.log('Processing Get request')
+    let data = JSON.parse(fs.readFileSync(notes, 'utf8'));
+    console.log(`Get request - Returning notes data:  ${JSON.stringify(data)}`);
+    response.JSON(data);
+});
